5ac57ee055860a26d41bb52f2d5dc95bb81dff37
unknown <roshiniadapa@gmail.com>
2020-06-13 12:50:04 +0530

    ab tests

--- backend/server/endpoints/urls.py
+++ backend/server/endpoints/urls.py
@@ -1,20 +1,26 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-
 from endpoints.views import EndpointViewSet
 from endpoints.views import MLAlgorithmViewSet
+from endpoints.views import MLAlgorithmStatusViewSet
 from endpoints.views import MLRequestViewSet
-from endpoints.views import PredictView # import PredictView
+from endpoints.views import PredictView
+from endpoints.views import ABTestViewSet
+from endpoints.views import StopABTestView
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r"endpoints", EndpointViewSet, basename="endpoints")
 router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
+router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
 router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+router.register(r"abtests", ABTestViewSet, basename="abtests")
 
 urlpatterns = [
     url(r"^api/v1/", include(router.urls)),
-    # add predict url
     url(
         r"^api/v1/(?P<endpoint_name>.+)/predict$", PredictView.as_view(), name="predict"
+    ),
+    url(
+        r"^api/v1/stop_ab_test/(?P<ab_test_id>.+)", StopABTestView.as_view(), name="stop_ab"
     ),
 ]
